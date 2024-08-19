773158711331303cc69d621129bfdb08e54fd339
ByteWisePrashast <161324477+ByteWisePrashast@users.noreply.github.com>
2024-08-19 20:07:24 +0000

    Variable basics

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+console.log("Prashast")
+const accountId= 2345;
+let accountEmail ="ram@gmail.com"
+var accountPassword = "1234"
+custmerAdd= "Kanpur"
+let accountState;
+
+// accountId =123  // not allowded
+
+
+console.log(accountId)
+accountEmail ="poiuy@gmail.com"
+accountPassword ="9876"
+custmerAdd ="Puri"
+
+/*
+prefer to use let instead of var because of the scope issue such as function or block
+scope
+*/
+
+console.table([ accountId , accountEmail , accountPassword , custmerAdd , accountState])
