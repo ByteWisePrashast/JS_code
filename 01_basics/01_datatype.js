46fdba0e3a7902dd73cf568214ae405ce835460d
ByteWisePrashast <161324477+ByteWisePrashast@users.noreply.github.com>
2024-08-21 20:30:32 +0000

    datatype

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all the js code as newer version
+// alert(3+3)  we are using node js not browser;
+
+console.log(3+3);
+
+let name ="Ram"
+let age=9
+let userLoggedIn = true
+
+
+
+//DATATYPE
+/*
+int 
+bigint 
+string => ""
+boolean => ture/false
+undefined
+null =>standalone value
+*/
+
+
+/*
+object
+*/
+ console.log(typeof age)
+ console.log(typeof null)
+ console.log(typeof undefined)
