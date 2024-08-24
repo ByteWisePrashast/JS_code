e8e2059ebaa7de72126aaea8f5dd42a8f5ed29be
ByteWisePrashast <161324477+ByteWisePrashast@users.noreply.github.com>
2024-08-24 07:36:37 +0000

    conversionOpertaion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,50 @@
+// let score= "33abs"
+// let score= null
+// let score= undefined
+// let score= true
+let score= "Ram"
+
+
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let intNumber = Number(score)
+// console.log(typeof intNumber)
+// console.log(intNumber)
+
+// "33" => 33
+// "33abs" =>NaN
+// true = 1
+// "Ram" = NaN
+
+
+// let isLoggedIn = 1
+let isLoggedIn = ""
+// let isLoggedIn = "Ram"
+let convertBoolean = Boolean(isLoggedIn)
+// console.log(convertBoolean)
+
+
+// true => 1   false =>0
+// "Ram" = true
+// "" =false
+
+
+let number= 333
+let convertString= String(number)
+// console.log(convertString)
+// console.log(typeof (convertString))
+
+
+// *******************Operation*****************
+let value =35
+let negValue =-value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*4)
+console.log(2**6)
+console.log(2/3)
+console.log(2%3)
